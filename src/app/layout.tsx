1223ad8f443e251c117eb54e2d6cd4f039dd518b
patiphan24 <patiphun2016@gmail.com>
2024-09-04 14:09:24 +0700

    end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Patiphan Leknok"
+            studentId="660610771"
           />
         </div>
       </body>
